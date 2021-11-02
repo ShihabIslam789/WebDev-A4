1bcee9c5c4d30aaeb7632de711f8de8643911266
Shihab Islam <Shihab.Islam90@myhunter.cuny.edu>
2021-11-01 20:10:57 -0400

    added more of the starter working code and added componets folder.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
+import UserProfile from "./components/UserProfile";
+import Login from "./components/Login";
+import "./App.css";
+import Debits from "./components/Debits";
+import Credits from "./components/Credits";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      accountBalance: 0,
+      currentUser: {
+        userName: "Doylan Mihov",
+        memberSince: "06/21/20",
+      },
+      debits: [],
+      credits: [],
+    };
+  }
+
+
 }
-
-export default App;
+  export default App;
